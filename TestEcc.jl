7bb6bf193d424fe2edc496d8fab02a2b0211c0c0
Nick <dohp.kim@gmail.com>
2022-01-25 23:15:58 +0900

    secp256k1

---
+++ TestEcc.jl
@@ -0,0 +1,27 @@
+using Test
+include("ecc.jl");  using .Ecc
+
+@testset "FieldElement: finite field and modulo" begin
+  a = FieldElement(7, 13)
+  b = FieldElement(12, 13)
+  c = FieldElement(6, 13)
+  @test a + b == c
+
+  d = FieldElement(3, 13)
+  e = FieldElement(12, 13)
+  f = FieldElement(10, 13)
+  @test d * e == f
+
+  pow1 = FieldElement(3, 13)
+  pow2 = FieldElement(1, 13)
+  @test pow1^3 == pow2
+
+  pow3 = FieldElement(7, 13)
+  pow4 = FieldElement(8, 13)
+  @test pow3^.-3 == pow4
+end
+
+@testset "Point: is a point on the secp256k1 curve" begin
+  @test_nowarn p1 = Point(-1, -1, 5, 7)
+  @test_throws AssertionError p2 = Point(-1, -2, 5, 7)
+end
--- ecc.jl
+++ ecc.jl
@@ -1,3 +1,6 @@
+module Ecc
+
+export FieldElement, Point
 
 import Base.isless
 import Base.isequal
@@ -9,10 +12,10 @@ import Base./
 import Base.inv
 
 struct FieldElement
-  num::Int64
-  prime::Int64
+  num::Integer
+  prime::Integer
 
-  function FieldElement(num::Int64, prime::Int64)
+  function FieldElement(num::Integer, prime::Integer)
     @assert(0 <= num <= (prime - 1), "Num $num not in a field range from 0 to $(prime - 1)")
     new(num, prime)
   end
@@ -23,39 +26,57 @@ function Base.show(io::IO, fe::FieldElement)
 end
 
 function isequal(fe1::FieldElement, fe2::FieldElement)
-  return (fe1.num, fe1.prime) == (fe2.num, fe2.prime)
+  (fe1.num, fe1.prime) == (fe2.num, fe2.prime)
 end
 
 function isless(fe1::FieldElement, fe2::FieldElement)
-  return (fe1.num, fe1.prime) < (fe2.num, fe2.prime)
+  (fe1.num, fe1.prime) < (fe2.num, fe2.prime)
 end
 
 function +(fe1::FieldElement, fe2::FieldElement)
   @assert(fe1.prime == fe2.prime, "Cannot add numbers in diffrent Fields")
   num = mod(fe1.num + fe2.num, fe1.prime)
-  return FieldElement(num, fe1.prime)
+  FieldElement(num, fe1.prime)
 end
 
 function -(fe1::FieldElement, fe2::FieldElement)
   @assert(fe1.prime == fe2.prime, "Cannot subtract numbers in diffrent Fields")
   num = mod(fe1.num - fe2.num, fe1.prime)
-  return FieldElement(num, fe1.prime)
+  FieldElement(num, fe1.prime)
 end
 
 function *(fe1::FieldElement, fe2::FieldElement)
   @assert(fe1.prime == fe2.prime, "Cannot multiply numbers in diffrent Fields")
   num = mod(fe1.num * fe2.num, fe1.prime)
-  return FieldElement(num, fe1.prime)
+  FieldElement(num, fe1.prime)
 end
 
 function ^(fe::FieldElement, exp::Integer)
   n = mod(exp, fe.prime - 1)
   num = powermod(fe.num, n, fe.prime)
-  return FieldElement(num, fe.prime)
+  FieldElement(num, fe.prime)
 end
 
 function /(fe1::FieldElement, fe2::FieldElement)
   @assert(fe1.prime == fe2.prime, "Cannot divide numbers in diffrent Fields")
   num = mod(fe1.num * powermod(fe2.num, fe1.prime - 2, fe1.prime), fe1.prime)
-  return FieldElement(num, fe1.prime)
+  FieldElement(num, fe1.prime)
 end
+
+struct Point
+  x::Integer
+  y::Integer
+  a::Integer
+  b::Integer
+
+  function Point(x::Integer, y::Integer, a::Integer, b::Integer)
+    @assert(y^2 == x^3 + a * x + b, "($x, $y) is not on the curve(secp256k1)")
+    new(x, y, a, b)
+  end
+end
+
+function isequal(p1::Point, p2::Point)
+  p1.x == p2.x && p1.y == p2.y && p1.a == p2.a && p1.b == p2.b
+end
+
+end # module
