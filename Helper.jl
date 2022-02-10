e8cfb58f88b7f3af494e272da4b1ccf87eadaabf
Nick <dohp.kim@gmail.com>
2022-02-10 22:16:54 +0900

    wip

--- Helper.jl
+++ Helper.jl
@@ -1,6 +1,6 @@
 module Helper
 
-export hash256, hash256toBigInt, big2hex
+export hash256, hash256toBigInt, big2hex, toByteArray, append
 
 using SHA
 
@@ -18,37 +18,54 @@ function big2hex(big::BigInt)::String
   string(big, base = 16)
 end
 
+function toByteArray(unit::Base.CodeUnits, multiple::Integer=1)::Vector{UInt8}
+  arr = (UInt8)[]
+  parsed = parse(UInt8, bytes2hex(unit))
+  for i in 1:multiple
+    push!(arr, parsed)
+  end
+  arr
+end
+
+function toByteArray(big::BigInt, len::Integer=32; bigEndian::Bool=true)::Vector{UInt8}
+  hexstring = big2hex(big)
+  arr = (UInt8)[]
+  index = 1
+  while index < sizeof(hexstring)
+    parsed = parse(UInt8, hexstring[index:index + 1], base = 16)
+    if bigEndian
+      push!(arr, parsed)
+    else
+      pushfirst!(arr, parsed)
+    end
+    index += 2
+  end
+  arr[1:len]
+end
+
+function append(a::Vector{UInt8}, b::Vector{UInt8})::Vector{UInt8}
+  arr = (UInt8)[]
+  append!(arr, a)
+  append!(arr, b)
+  arr
+end
+
 
 # RFC6979
 function deterministicK(pk, z::BigInt)::BigInt
-  b00 = parse(UInt8, bytes2hex(b"\x00"))
-  b01 = parse(UInt8, bytes2hex(b"\x01"))
-  k = Array([b00])
-  v = Array([b01])
-  for i in 1:31
-    push!(k, b00)
-    push!(v, b01)
-  end
+  k = toByteArray(b"\x00", 32)
+  v = toByteArray(b"\x01", 32)
 
   if z > N
     z -= N
   end
 
-  zBytes = codeunits(big2hex(z))
-  secretBytes = codeunits(big2hex(pk.secret))
+  zBytes = toByteArray(z)
+  secretBytes = toByteArray(pk.secret)
 
-  vCopy = copy(v)
-  push!(vCopy, b00)
-  append!(vCopy, secretBytes)
-  append!(vCopy, zBytes)
-  k = hmac_sha256(k, vCopy)
+  k = hmac_sha256(k, append(append(v, secretBytes), zBytes))
   v = hmac_sha256(k, v)
-
-  vCopy2 = copy(v)
-  push!(vCopy2, b01)
-  append!(vCopy2, secretBytes)
-  append!(vCopy2, zBytes)
-  k = hmac_sha256(k, vCopy2)
+  k = hmac_sha256(k, append(append(v, secretBytes), zBytes))
   v = hmac_sha256(k, v)
 
   while true
@@ -64,44 +81,6 @@ function deterministicK(pk, z::BigInt)::BigInt
   end
 end
 
-bit = parse(UInt8, bytes2hex(b"\x00"))
-byteArr = [bit]
-arr = (UInt8)[]
-
-b00 = b"\x00"
-println(typeof(b00))
-println(b00)
-
-z = 0xbc62d4b80d9e36da29c16c5d4d9f11731f36052c72401a76c23c0fb5a9b74423
-
-str = "ff"
-strParsed = parse(UInt8, str, base = 16)
-
-function toByteArray(unit::Base.CodeUnits, len::Integer=32; bigendian::Bool=true)::Vector{UInt8}
-  [parse(UInt8, bytes2hex(unit))]
-end
-
-function toByteArray(big::BigInt, len::Integer=32; bigEndian::Bool=true)
-  hexstring = big2hex(big)
-  arr = (UInt8)[]
-  index = 1
-  while index < sizeof(hexstring)
-    chunk = hexstring[index:index+1]
-    parsed = parse(UInt8, chunk, base = 16)
-    if bigEndian
-      push!(arr, parsed)
-    else
-      pushfirst!(arr, parsed)
-    end
-    index += 2
-  end
-  arr[1:len]
-end
-
-rs = toByteArray(z)
-println(rs)
-println(typeof(rs))
-println(length(rs))
 
 
 end # module
