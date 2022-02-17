bd3bc4fa2a805a00164516e8b4b25cd9136fcf5c
Nick <dohp.kim@gmail.com>
2022-02-17 22:18:30 +0900

    WIP: parsing Transaction

--- Scripts.jl
+++ Scripts.jl
@@ -3,13 +3,22 @@ module Scripts
 
 export Script, parseScript
 
+include("Helper.jl");  using .Helper
 struct Script
 end
 
-function parseScript(io::IOStream)::Union{Script, Nothing}
-  
-  return nothing
+function parseScript(io::IOStream)::Script
+  return Script()
 end
 
+function serializeScriptSig(s::Script)::Vector{UInt8}
+  # TODO:
+  toByteArray(0x42)
+end
+
+function serializeScriptPubKey(s::Script)::Vector{UInt8}
+  # TODO:
+  toByteArray(0x42)
+end
 
 end # module
--- Transaction.jl
+++ Transaction.jl
@@ -1,6 +1,6 @@
 module Transaction
   
-export Tx, TxIn, TxOut, id, hash, parseTx
+export Tx, TxIn, TxOut, id, hash, parseTx, parseTxIn, parseTxOut
 
 include("Scripts.jl");  using .Scripts
 include("Helper.jl");  using .Helper
@@ -11,13 +11,8 @@ struct TxIn
   scriptSig::Script
   sequence::UInt32
 
-  function TxIn(prevTx::Vector{UInt8}, prevIndex::UInt32, scriptSig::Script=nothing; sequence::UInt32=0xffffffff)
-    script
-    if scriptSig === nothing
-      script = Script()
-    else
-      script = scriptSig
-    end
+  function TxIn(prevTx::Vector{UInt8}, prevIndex::Integer, scriptSig::Script=nothing; sequence::Integer=0xffffffff)
+    script = scriptSig === nothing ? Script() : scriptSig
     new(prevTx, prevIndex, script, sequence)
   end
 end
@@ -27,22 +22,40 @@ function Base.show(io::IO, t::TxIn)
 end
 
 struct TxOut
+  amount::BigInt
+  scriptPubKey::Script
+
+  function TxOut(amount::BigInt, scriptPubKey::Script)
+    new(amount, scriptPubKey)
+  end
+end
+
+function Base.show(io::IO, t::TxOut)
+  print(io, "$(t.amount):$(t.scriptPubKey)")
 end
 
 struct Tx
-  version
+  version::UInt32
   txIns::Vector{TxIn}
   txOuts::Vector{TxOut}
-  locktime
+  locktime::UInt32
   testnet::Bool
 
-  function Tx(version, txIns, txOuts, locktime; testnet::Bool=false)
+  function Tx(version::Integer, txIns::Vector{TxIn}, txOuts::Vector{TxOut}, locktime::Integer; testnet::Bool=false)
     new(version, txIns, txOuts, locktime, testnet)
   end
 end
 
 function Base.show(io::IO, t::Tx)
-  print(io, t)
+  txInString = ""
+  for item in t.txIns
+    txInString *= "$(bytes2hex(item.prevTx)):$(item.prevIndex)\n"
+  end
+  txOutString = ""
+  for item in t.txOuts
+    txOutString *= "$(item.amount):$(item.scriptPubKey)\n"
+  end
+  print(io, "tx: \nversion: $(t.version)\ntx_ins:\n$(txInString)tx_out:\n$(txOutString)locktime: $(t.locktime)")
 end
 
 function id(t::Tx)
@@ -53,25 +66,38 @@ function hash(t::Tx)
   
 end
 
-function parseTx(io::IOStream)
+function parseTx(io::IOStream)::Tx
   serializedVersion = htol(read(io, 4))
   numInputs = decodeVarints(io)
   txIns = (TxIn)[]
   for i in 1:numInputs
-    append!(txIns, parseTxIn(io))
+    push!(txIns, parseTxIn(io))
   end
-
-
-
-  @show serializedVersion
+  numOutouts = decodeVarints(io)
+  txOuts = (TxOut)[]
+  for i in 1:numOutouts
+    push!(txOuts, parseTxOut(io))
+  end
+  locktime = htol(read(io, 4))
+  Tx(bytes2big(serializedVersion), txIns, txOuts, bytes2big(locktime))
 end
 
 function parseTxIn(io::IOStream)::TxIn
   prevTx = htol(read(io, 32))
   prevIndex = htol(read(io, 4))
   scriptSig = parseScript(io)
-  sequence = read(io, 4)
-  TxIn(prevTx, prevIndex, scriptSig, sequence = sequence)
+  sequence = htol(read(io, 4))
+  TxIn(prevTx, bytes2big(prevIndex), scriptSig, sequence = bytes2big(sequence))
+end
+
+function parseTxOut(io::IOStream)::TxOut
+  amount = htol(read(io, 8))
+  scriptPubKey = parseScript(io)
+  TxOut(bytes2big(amount), scriptPubKey)
+end
+
+function serializeTxOut(t::TxOut)
+    
 end
 
 end # module
