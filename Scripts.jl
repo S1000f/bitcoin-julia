902e1cefa221d8544715b4ff356794870bc4c581
Nick <dohp.kim@gmail.com>
2022-03-29 23:46:21 +0900

    parse Script

--- Scripts.jl
+++ Scripts.jl
@@ -1,14 +1,48 @@
-
 module Scripts
 
 export Script, parseScript
 
 include("Helper.jl");  using .Helper
+
 struct Script
+  cmds::Vector{UInt8}
+
+  function Script(cmds=nothing)
+    new(cmds === nothing ? (UInt8)[] : cmds)
+  end
 end
 
-function parseScript(io::IO)::Script
-  return Script()
+function parseScript(s::IO)::Script
+  len = decodeVarints(s)
+  cmds = (UInt8)[]
+  count = 0
+  while count < len
+    current = read(s, 1)
+    count += 1
+    currentByte = current[1]
+    if 1 <= currentByte <= 75
+      n = currentByte
+      append!(cmds, read(s, n))
+      count += n
+    elseif currentByte == 76
+      dataLength = bytes2big(read(s, 1); bigEndian=false) 
+      append!(cmds, read(s, dataLength))
+      count += dataLength + 1
+    elseif currentByte == 77
+      dataLength = bytes2big(read(s, 2); bigEndian=false) 
+      append!(cmds, read(s, dataLength))
+      count += dataLength + 2
+    else
+      opCode = currentByte
+      append!(cmds, opCode)
+    end
+  end
+
+  if count != len
+    error("parsing script failed")
+  end
+  
+  return Script(cmds)
 end
 
 function serializeScriptSig(s::Script)::Vector{UInt8}
---
+++ TestScripts.jl
@@ -0,0 +1,14 @@
+using Test
+
+include("Scripts.jl");  using .Scripts
+include("Helper.jl");  using .Helper
+
+s = Script()
+println(s)
+
+s2 = Script([0x42, 0xff])
+println(s2)
+
+arr = [0x0a]
+num = arr[1]
+println(typeof(num), " ", num)
