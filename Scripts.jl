f8ab777d6350b05108e70b1f26db41e89ed259c9
Nick <dohp.kim@gmail.com>
2022-04-11 23:42:14 +0900

    p2pk

--- Scripts.jl
+++ Scripts.jl
@@ -3,6 +3,8 @@ module Scripts
 export Script, parseScript, serialize
 
 include("Helper.jl");  using .Helper
+include("Op.jl");  using .Op
+using Logging
 
 import Base.+
 
@@ -52,7 +54,7 @@ function rawSerialize(s::Script)::Vector{UInt8}
   result = (UInt8)[]
 
   for cmd in s.cmds
-    if cmd isa Integer
+    if cmd isa Int
       append!(result, toByteArray(cmd, 1, bigEndian=false))
     else
       len = length(cmd)
@@ -80,6 +82,51 @@ end
 
 function +(s1::Script, s2::Script)::Script
   Script(append(s1.cmds, s2.cmds))
+end
+
+function evaluate(s::Script, z::BigInt)::Bool
+  cmds = s.cmds[:]
+  stack = []
+  altstack = []
+
+  while length(cmds) > 0
+    cmd = pop!(cmds)
+
+    if cmd isa Int
+        operation = OP_CODE_FUNCTIONS[cmd]
+      if cmd in (99, 100)
+        if !operation(stack, cmds)
+          @info "bad op: $(OP_CODE_NAMES[cmd])"
+          return false
+        end
+      elseif cmd in (107, 108)
+        if !operation(stack, altstack)
+          @info "bad op: $(OP_CODE_NAMES[cmd])"
+          return false
+        end
+      elseif cmd in (172, 173, 174, 175)
+        if !operation(stack, z)
+          @info "bad op: $(OP_CODE_NAMES[cmd])"
+          return false
+        end
+      else
+        if !operation(stack)
+          @info "bad op: $(OP_CODE_NAMES[cmd])"
+          return false
+        end
+      end
+    else
+      append!(stack, cmd)
+    end
+  end
+
+  if length(stack) == 0
+    return false
+  end
+
+  if isempty(stack)
+    return false
+  end
 end
 
 end # module
